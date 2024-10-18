49da8753b86a1dfcdee56b46a2957c0b949d6176
Gautham Mahadevan <128295307+gauthamgtg@users.noreply.github.com>
2024-10-18 11:54:54 +0530

    added ind and us spend

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from curses.ascii import alt
+from datetime import date, datetime, timedelta
 from urllib.error import URLError
 import pandas as pd
-import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
 import psycopg2
@@ -15,7 +15,10 @@ name=st.secrets["name"]
 passw=st.secrets["passw"]
 server=st.secrets["server"]
 port=st.secrets["port"]
-
+st.set_page_config( page_title = "Spend Stats",
+    page_icon=":bar_chart:",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
 def redshift_connection(dbname, user, password, host, port):
     def decorator(func):
@@ -54,16 +57,16 @@ def redshift_connection(dbname, user, password, host, port):
 
 query = '''
 with spends AS
-    (SELECT cast(aas.euid as float) as euid ,ad_account_id,date(date_start) as dt,sum(spend)spend
+    (SELECT max(cast(aas.euid as float)) as euid ,ad_account_id,date(date_start) as dt,sum(spend)spend
     from  ad_account_spends aas 
-    group by 1,2,3),
+    group by 2,3),
     total_payment AS
         (select cast(td.euid as float)euid,ad_account,sum(adspend_amount) total_paid 
         from payment_trans_details td
         group by 1,2)
 
 select eu.euid,eu.business_name,eu.company_name,dt,efaa.ad_account_name,
-efaa.flag as currency_code,a.ad_account_id,total_spent,total_paid,spend,curr_month_spend,
+case when efaa.flag is null then 'INR' else efaa.flag end as currency_code,a.ad_account_id,total_spent,total_paid,spend,curr_month_spend,
 case when lower(eu.company_name) like '%datong%' or lower(eu.company_name) like '%omaza%' then 'Datong' 
         when eu.euid in (2201,2168,2202,2181,2051,2100,2310,2309,2281) then 'FB Boost'
         when eu.euid in (1911)then 'Adfly' 
@@ -92,17 +95,18 @@ from enterprise_users eu
     on cms.euid=eu.euid and cms.ad_account_id=a.ad_account_id
     left join total_payment b
     on  b.euid=eu.euid and concat('act_', b.ad_account)=a.ad_account_id
-    left join enterprise_facebook_account efa on efa.euid = eu.euid 
     left join 
-    (
-    select ad_account_id,ad_account_name,
-        CASE WHEN currency IS NULL OR LOWER(currency) = 'inr' THEN 'India' 
-        ELSE 'US' 
-        END AS flag,
-    max(expiry_date) as expiry_date,max(spend_cap) as spend_cap 
-    from enterprise_facebook_ad_account
-    group by 1,2,3
-    )efaa
+        (
+            SELECT ad_account_id,ad_account_name,currency as flag
+            FROM
+            (
+            select ad_account_id,ad_account_name,fb_user_id,row_number() over(partition by ad_account_id order by fb_user_id) as rank,currency
+            from 
+            (select * from enterprise_facebook_ad_account
+            where status='true')a
+            )a
+            where rank=1 
+        ) efaa
     on concat('act_',efaa.ad_account_id)= a.ad_account_id
     where a.ad_account_id is not null
     order by euid desc
@@ -119,7 +123,6 @@ def execute_query(connection, cursor,query):
 
 df = execute_query(query=query)
 
-
 #chaning proper format of date
 df['dt'] = pd.to_datetime(df['dt']).dt.date
 
@@ -128,13 +131,18 @@ df['spend'] = pd.to_numeric(df['spend'], errors='coerce')
 df['euid'] = pd.to_numeric(df['euid'], errors='coerce')
 grouped_data_adacclevel = None
 pivoted_data_adacclevel = None
+# Calculate yesterday and day before yesterday's dates
+yesterday = (date.today() - timedelta(days=1))
+day_before_yst = (date.today() - timedelta(days=2))
+current_month = datetime.now().month
+current_year = datetime.now().year
 
 
 with st.sidebar:
     selected = option_menu(
         menu_title="Navigation",  # Required
-        options=["Login","Main Page", "Raw Data"],  # Required
-        icons=["lock","house", "table"],  # Optional: icons from the Bootstrap library
+        options=["Login","Main Page", "Raw Data","Overall Stats - Ind","Overall Stats - US"],  # Required
+        icons=["lock","house", "table","currency-rupee",'currency-dollar'],  # Optional: icons from the Bootstrap library
         menu_icon="cast",  # Optional: main menu icon
         default_index=0,  # Default active menu item
     )
@@ -198,6 +206,8 @@ if selected == "Main Page" and st.session_state.status == "verified":
     col2.subheader("Start Date : " + str(filtered_df['dt'].min()))
     col3.subheader("Last Active Date : " + str(filtered_df['dt'].max()))
 
+   
+
     # Assuming your 'dt' column is already in date format (e.g., YYYY-MM-DD)
     if grouping == 'Year':
         filtered_df.loc[:, 'grouped_date'] = filtered_df['dt'].apply(lambda x: x.strftime('%Y'))  # Year format as 2024
@@ -218,9 +228,7 @@ if selected == "Main Page" and st.session_state.status == "verified":
     # Step 6: Display the table of top_customers_flag and grouped spend values
     st.write(f"Grouped Spend for Top Customers Flag: {selected_flag}")
     pivots_df = grouped_df.pivot(index='euid', columns='grouped_date', values='spend')
-
     # st.button("Transpose", type="primary")
-
 # Initialize session state for button press
     if 'show_transposed' not in st.session_state:
         st.session_state['show_transposed'] = False  # Default to showing non-transposed data
@@ -279,3 +287,213 @@ elif selected == "Raw Data" and st.session_state.status == "verified":
         st.write(pivoted_data_adacclevel)  # Show original
 
     st.write(st.session_state['grouped_data_adacclevel'])
+
+
+elif selected == "Overall Stats - Ind" and st.session_state.status == "verified":
+
+    st.title("Overall Stats Page - India")
+
+    indian_df = df[df['currency_code'].str.lower() == 'inr']
+    
+    # Group the DataFrame by 'euid', 'ad_account_id', and 'dt', and sum 'spend'
+    ind_grouped_data_adacclevel = indian_df.groupby(['euid', 'ad_account_id', 'dt'])['spend'].sum().reset_index()
+
+
+    # Filter data for yesterday and day before yesterday
+    ind_yesterday_data = ind_grouped_data_adacclevel[ind_grouped_data_adacclevel['dt'] == yesterday]
+    ind_day_before_yst_data = ind_grouped_data_adacclevel[ind_grouped_data_adacclevel['dt'] == day_before_yst]
+
+    # Calculate the total spend for each day
+    ind_yst_spend = ind_yesterday_data['spend'].sum()
+    ind_day_before_yst_spend = ind_day_before_yst_data['spend'].sum()
+
+    # Calculate the number of unique ad_account_id for each day
+    ind_num_ad_accounts_yesterday = ind_yesterday_data['ad_account_id'].nunique()
+    ind_num_ad_accounts_day_before_yst = ind_day_before_yst_data['ad_account_id'].nunique()
+
+    # Ensure we have data to avoid division by zero
+    if ind_day_before_yst_spend > 0:
+        ind_spend_change = round(((ind_yst_spend - ind_day_before_yst_spend) / ind_day_before_yst_spend) * 100, 2)
+    else:
+        ind_spend_change = 0
+
+    if ind_num_ad_accounts_day_before_yst > 0:
+        ind_ad_account_change = round(((ind_num_ad_accounts_yesterday - ind_num_ad_accounts_day_before_yst) / ind_num_ad_accounts_day_before_yst) * 100, 2)
+    else:
+        ind_ad_account_change = 0
+
+    # Calculate average spend per ad account for yesterday and day before yesterday
+    if ind_num_ad_accounts_yesterday > 0:
+        ind_avg_spend_per_account_yesterday = ind_yst_spend / ind_num_ad_accounts_yesterday
+    else:
+        ind_avg_spend_per_account_yesterday = 0
+
+    if ind_num_ad_accounts_day_before_yst > 0:
+        ind_avg_spend_per_account_day_before = ind_day_before_yst_spend / ind_num_ad_accounts_day_before_yst
+    else:
+        ind_avg_spend_per_account_day_before = 0
+
+    # Calculate the change in average spend per ad account
+    if ind_avg_spend_per_account_day_before > 0:
+        avg_ind_spend_change = round(((ind_avg_spend_per_account_yesterday - ind_avg_spend_per_account_day_before) / ind_avg_spend_per_account_day_before) * 100, 2)
+    else:
+        avg_ind_spend_change = 0
+
+    # Display the metrics
+    col1, col2, col3 = st.columns(3)
+
+    # Metric 1: Yesterday's Spend and change %
+    col1.metric("Yesterday Spend", f"₹{ind_yst_spend}", f"{ind_spend_change}%")
+
+    # Metric 2: Number of Ad Accounts and change %
+    col2.metric("Ad Accounts", ind_num_ad_accounts_yesterday, f"{ind_ad_account_change}%")
+
+    # Metric 3: Average Spend per Ad Account and change %
+    col3.metric("Avg Spend per Ad Account", f"₹{round(ind_avg_spend_per_account_yesterday, 2)}", f"{avg_ind_spend_change}%")
+
+    st.dataframe(indian_df)
+    st.dataframe(ind_grouped_data_adacclevel)
+
+    st.write('Day level spends')
+    ind_grouped_data = indian_df.groupby(['dt'])['spend'].sum().reset_index().sort_values(by='dt', ascending=False)
+    st.dataframe(ind_grouped_data)
+
+    st.line_chart(ind_grouped_data, x='dt', y='spend')
+
+
+elif selected == "Overall Stats - US" and st.session_state.status == "verified":
+
+    st.title("Overall Stats - US")
+
+    us_df = df[df['currency_code'].str.lower() != 'inr']
+
+    us_df = us_df.loc[:, us_df.columns != 'company_name']
+
+    # Identify unique currency codes other than 'USD'
+    non_usd_currencies = us_df['currency_code'].unique()
+    non_usd_currencies = [currency for currency in non_usd_currencies if currency != 'USD']
+
+    # Create a dictionary to store the conversion rates entered by the user
+    conversion_rates = {}
+
+    # Predefine default values for specific currencies
+    default_values = {
+                        'EUR': 1.1,
+                        'GBP': 1.3,
+                        'AUD': 0.75
+                    }
+
+    # Display input boxes for each unique currency code other than 'USD'
+    st.write("Enter conversion rates for the following currencies:")
+   
+    # Create columns dynamically based on the number of currencies
+    cols = st.columns(3)  # Adjust the number of columns (3 in this case)
+
+    # Iterate over non-USD currencies and display them in columns
+    for idx, currency in enumerate(non_usd_currencies):
+        default_value = default_values.get(currency, 1.0)  # Use default value if defined, otherwise 1.0
+        with cols[idx % 3]:  # Rotate through the columns
+            conversion_rates[currency] = st.number_input(
+                f"{currency} to USD:", value=default_value, min_value=0.0, step=0.001, format="%.3f"
+            )
+
+    # Function to convert the spend to USD using the entered conversion rates
+    def convert_to_usd(row):
+        if row['currency_code'] == 'USD':
+            return row['spend']
+        elif row['currency_code'] in conversion_rates:
+            return row['spend'] / conversion_rates[row['currency_code']]
+        return row['spend']
+
+    # Create the 'spend_in_usd' column
+    us_df['spend_in_usd'] = us_df.apply(lambda row: convert_to_usd(row), axis=1)
+
+    
+    # Group the DataFrame by 'euid', 'ad_account_id', and 'dt', and sum 'spend'
+    us_grouped_data_adacclevel = us_df.groupby([ 'ad_account_id', 'dt'])['spend_in_usd'].sum().reset_index()
+
+    # Filter data for yesterday and day before yesterday
+    us_yesterday_data = us_grouped_data_adacclevel[us_grouped_data_adacclevel['dt'] == yesterday]
+    us_day_before_yst_data = us_grouped_data_adacclevel[us_grouped_data_adacclevel['dt'] == day_before_yst]
+
+    # Calculate the total spend for each day
+    us_yst_spend = round(us_yesterday_data['spend_in_usd'].sum(),2)
+    us_day_before_yst_spend = us_day_before_yst_data['spend_in_usd'].sum()
+
+    # Calculate the number of unique ad_account_id for each day
+    us_num_ad_accounts_yesterday = us_yesterday_data['ad_account_id'].nunique()
+    us_num_ad_accounts_day_before_yst = us_day_before_yst_data['ad_account_id'].nunique()
+
+    # Ensure we have data to avoid division by zero
+    if us_day_before_yst_spend > 0:
+        us_spend_change = round(((us_yst_spend - us_day_before_yst_spend) / us_day_before_yst_spend) * 100, 2)
+    else:
+        us_spend_change = 0
+
+    if us_num_ad_accounts_day_before_yst > 0:
+        us_ad_account_change = round(((us_num_ad_accounts_yesterday - us_num_ad_accounts_day_before_yst) / us_num_ad_accounts_day_before_yst) * 100, 2)
+    else:
+        us_ad_account_change = 0
+
+    # Calculate average spend per ad account for yesterday and day before yesterday
+    if us_num_ad_accounts_yesterday > 0:
+        us_avg_spend_per_account_yesterday = us_yst_spend / us_num_ad_accounts_yesterday
+    else:
+        us_avg_spend_per_account_yesterday = 0
+
+    if us_num_ad_accounts_day_before_yst > 0:
+        us_avg_spend_per_account_day_before = us_day_before_yst_spend / us_num_ad_accounts_day_before_yst
+    else:
+        us_avg_spend_per_account_day_before = 0
+
+    # Calculate the change in average spend per ad account
+    if us_avg_spend_per_account_day_before > 0:
+        us_avg_spend_change = round(((us_avg_spend_per_account_yesterday - us_avg_spend_per_account_day_before) / us_avg_spend_per_account_day_before) * 100, 2)
+    else:
+        us_avg_spend_change = 0
+
+    
+    # Filter the DataFrame to get the current month’s data
+    current_month_df = us_df[
+        (pd.to_datetime(us_df['dt']).dt.month == current_month) &
+        (pd.to_datetime(us_df['dt']).dt.year == current_year)
+    ]
+
+    # Calculate the total spend for the current month
+    total_current_month_spend = current_month_df['spend_in_usd'].sum()
+
+
+    # Display the metrics
+    col1, col2, col3, col4 = st.columns(4)
+
+    # Metric 1: Yesterday's Spend and change %
+    col1.metric("Yesterday Spend", f"${us_yst_spend}", f"{us_spend_change}%")
+
+    # Metric 2: Number of Ad Accounts and change %
+    col2.metric("Ad Accounts", us_num_ad_accounts_yesterday, f"{us_ad_account_change}%")
+
+    # Metric 3: Average Spend per Ad Account and change %
+    col3.metric("Avg Spend per Ad Account", f"${round(us_avg_spend_per_account_yesterday, 2)}", f"{us_avg_spend_change}%")
+
+    # Display the current month spend as a metric
+    col4.metric(label="Current Month Spend (in USD)", value=f"${total_current_month_spend:,.2f}")
+
+    st.write("Yesterday spend data:")
+    st.dataframe(us_df[us_df['dt']==yesterday])
+
+    st.write("Current Month spend data:")
+    st.dataframe(current_month_df)
+
+    # Display the updated DataFrame
+    st.write("Full Table with Spend in USD:")
+    st.dataframe(us_df)
+
+    # Display the updated DataFrame
+    st.write("Updated Table with Spend in USD:")
+    st.dataframe(us_grouped_data_adacclevel)
+
+    st.write('Day level spends')
+    us_grouped_data = us_df.groupby(['dt'])['spend_in_usd'].sum().reset_index().sort_values(by='dt', ascending=False)
+    st.dataframe(us_grouped_data)
+
+    st.line_chart(us_grouped_data, x='dt', y='spend_in_usd')
